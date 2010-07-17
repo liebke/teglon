0e5944b667606216e2ca9f67a4ec591c490c283d
David Edgar Liebke <liebke@gmail.com>
2010-07-17 19:10:53 -0400

    More work on web-api.

--- project.clj
+++ project.clj
@@ -2,14 +2,16 @@
   :description "Teglon provides Clojure and RESTful APIs for managing, browsing, and searching Maven repositories."
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-		 
+		 ;; needed for teglon.repo
 		 [org.apache.maven/maven-ant-tasks "2.0.10"]
                  [org.apache.maven/maven-artifact-manager "2.2.1"]
                  [org.apache.maven/maven-model "2.2.1"]
                  [org.apache.maven/maven-project "2.2.1"]
                  [org.apache.maven.wagon/wagon-file "1.0-beta-6"]
-		 
+		 ;; needed for teglon.web
 		 [compojure "0.4.1"]
-		 [ring "0.2.5"]]
+		 [ring "0.2.5"]
+		 ;; needed for teglon.github
+		 [clojure-http-client "1.1.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :main teglon.main)
--- src/teglon/web.clj
+++ src/teglon/web.clj
@@ -4,7 +4,9 @@
   teglon.web
   (:require [teglon.repo :as repo]
 	    [teglon.db :as db]
-	    [teglon.pages :as pages])
+	    [teglon.pages :as pages]
+	    [compojure.route :as route]
+	    [clojure.string :as s])
   (:use compojure.core
 	[clojure.contrib.json :only (pprint-json json-str)]
 	[ring.adapter.jetty :only (run-jetty)]
@@ -18,41 +20,95 @@
 
 ;; (start-server "/tmp/teglon/repo")
 ;; (stop-server)
-;; http://localhost:8080/api/v1/json/tree/show/incanter/incanter-core/1.2.3-SNAPSHOT
-;; http://localhost:8080/api/v1/json/tree/show/incanter/incanter-core
-;; http://localhost:8080/api/v1/json/tree/show/incanter
-;; http://localhost:8080/api/v1/json/tree/show
-;; http://localhost:8080/api/v1/json/tree/search?q=incanter
-;; http://localhost:8080/api/v1/json/tree/children/show/incanter/incanter-core/1.2.3-SNAPSHOT
 
-(defroutes main-routes
-  (GET "/api/v1/json/tree/show/:group/:name/*" [group name & version]
-       (json-str (db/get-model group name (version "*"))))
-  (GET "/api/v1/json/tree/show/:group/:name" [group name]
-       (json-str (db/list-all-versions-of-model group name)))
-  (GET "/api/v1/json/tree/show/:group" [group]
-       (json-str (db/list-models-by-group group)))
-  (GET "/api/v1/json/tree/show" []
-       (json-str (db/list-all-models)))
-  (GET "/api/v1/json/tree/search" [q]
-       (json-str (db/search-repo q)))
-  (GET "/api/v1/json/tree/children/show/:group/:name/*" [group name & version]
-       (json-str (db/list-models-that-depend-on group name (version "*"))))
-  (GET "/echo" request (prn-str request))
-  (ANY "*" []
-       {:status 404
-	:headers {"Content-Type" "text/html"}
-	:body "<h1>Page not found</h1>"}))
+(def *json-base-url* "/api/v1/json")
+(def *json-model-show-url* (str *json-base-url* "/model/show"))
+
+(defn model-to-uri
+  ([model]
+     (model-to-uri *json-model-show-url* model))
+  ([base-url model]
+     (let [{:keys [group name version]} model]
+       (str base-url "/" group "/" name "/" version))))
+
+(defn add-uri-to-model
+  ([model]
+     (assoc model :uri (model-to-uri model)))
+  ([base-url model]
+     (assoc model :uri (model-to-uri base-url model))))
+
+(defn parse-group-name-version [group-name-version]
+  (let [gnv-seq (s/split group-name-version #"/")
+	group (apply str (interpose "." (drop-last 2 gnv-seq)))
+	name (first (take-last 2 gnv-seq))
+	version (last gnv-seq)]
+    [group name version]))
+
+(defn parse-group-name [group-name]
+  (let [gn-seq (s/split group-name #"/")
+	group (apply str (interpose "." (drop-last gn-seq)))
+	name (last gn-seq)]
+    [group name]))
+
+(defn json-model-show [artifact-id]
+  (json-str (apply db/get-model
+		   (parse-group-name-version artifact-id))))
+
+(defn json-group-show [group]
+  (json-str (map add-uri-to-model
+		 (db/list-models-by-group group))))
+
+(defn json-versions-show [group-name]
+  (json-str (map add-uri-to-model
+		 (apply db/list-all-versions-of-model
+		    (parse-group-name group-name)))))
+
+(defn json-models-children [artifact-id]
+  (json-str (map add-uri-to-model
+		 (apply db/list-models-that-depend-on
+		    (parse-group-name-version artifact-id)))))
+
+(defn json-versions-children [group-name]
+  (json-str (map add-uri-to-model
+		 (apply db/list-models-that-depend-on
+		    (parse-group-name group-name)))))
+
+(defn json-group-children [group]
+  (json-str (map add-uri-to-model
+		 (db/list-models-that-depend-on group))))
+
+(defn json-models-show []
+  (json-str (map add-uri-to-model
+		 (db/list-all-models))))
+
+(defn json-models-search [query]
+  (json-str (map add-uri-to-model
+		 (db/search-repo query))))
+
+(defn teglon-app [repo-dir]
+  (routes
+   (GET *json-model-show-url* [& artifact-id]
+	(json-model-show (artifact-id "*")))
+   (GET (str *json-base-url* "/versions/show/*") [& group-name]
+	(json-versions-show (group-name "*")))
+   (GET (str *json-base-url* "/group/show/*") [& group]
+	(json-group-show (group "*")))
+   (GET (str *json-base-url* "/models/show") []
+	(json-models-show))
+   (GET (str *json-base-url* "/models/search") [q]
+	(json-models-search q))
+   (GET (str *json-base-url* "/children/model/show/*") [& artifact-id]
+	(json-models-children (artifact-id "*")))
+   (GET (str *json-base-url* "/children/versions/show/*") [& group-name]
+	(json-versions-children (group-name "*")))
+   (GET (str *json-base-url* "/children/group/show/*") [& group]
+	(json-group-children (group "*")))
+   (GET "/repo*/" request "<h1>Directory without an index file</h1>")
+   (route/files "/repo" {:root repo-dir})
+   (route/not-found "<h1>Move along</h1>")
+   (GET "/echo" request (prn-str request))))
 
 (def *server* (ref nil))
-
-(defn init-web-server
-  ([]
-     (init-web-server (repo/default-repo-dir)))
-  ([repo-dir]
-     (wrap! main-routes
-	    (:file repo-dir)
-	    (:file-info))))
 
 (defn start-server
   ([] (start-server (repo/default-repo-dir)))
@@ -60,9 +116,8 @@
      (let [port (or port 8080)]
        (println "Initializing Teglon server...")
        (repo/init-repo repo-dir)
-       (init-web-server repo-dir)
        (println (str "Starting webserver on port " port "..."))
-       (dosync (ref-set *server* (run-jetty #'main-routes
+       (dosync (ref-set *server* (run-jetty (teglon-app repo-dir)
 					    {:port port
 					     :join? false})))
        (println "Web server started."))))
